1d6936d15c73e15cbd1fd689caeb3f99cc60f7c8
刘荣歆 <zsmj1994@gmail.com>
2017-07-25 18:37:45 +0800

    add

---
+++ src/jsonOutline.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var vscode = require('vscode');
+var json = require('jsonc-parser');
+var path = require('path');
+
+class JsonOutlineProvider {
+
+	constructor() {
+		vscode.workspace.onDidChangeTextDocument(e => {})
+		this.parseTree();
+	}
+
+	parseTree() {
+		this.tree = null;
+		this.editor = vscode.window.activeTextEditor;
+		if (this.editor && this.editor.document && this.editor.document.languageId === 'json') {
+			this.tree = json.parseTree(this.editor.document.getText());
+		}
+		console.log(this.tree);
+	}
+
+}
+
+module.exports = JsonOutlineProvider;
