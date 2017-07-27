468d62da0f2456ef4775a07af3b7e9eb72ef302f
刘荣歆 <zsmj1994@gmail.com>
2017-07-27 15:06:13 +0800

    add

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
-var JsonOutlineProvider = require('./jsonOutline');
+var JsonOutlineProvider = require('./src/jsonOutline');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,17 +11,17 @@ function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "file-structure" is now active!');
-    console.log(context);
-    const jsonOutlineProvider = new JsonOutlineProvider(context);
-    vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
-
+    let jsonOutlineProvider = new JsonOutlineProvider(context);
+    vscode.window.registerTreeDataProvider('fileStructure', jsonOutlineProvider);
+    jsonOutlineProvider.onDidChangeTreeData(function (e) {
+        console.log('onDidChangeTreeData');
+    });
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand("fileStructure.refreshEntry", function () {
-        vscode.window.showInformationMessage("既然你已出价     !!!!");
+    vscode.commands.registerCommand('fileStructure.refreshEntry', range => {
+        console.log('fileStructure.refreshEntry');
     });
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
